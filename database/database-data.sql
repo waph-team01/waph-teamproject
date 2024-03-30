ae6c3d7e325561255088bdc0dd0284e296993aa8
pashamsp <155928223+pashamsp@users.noreply.github.com>
2024-03-30 04:40:53 -0400

    Update database-data.sql

--- database/database-data.sql
+++ database/database-data.sql
@@ -2,6 +2,6 @@ drop table if exists `users`;
 create table users(
 username varchar(50) PRIMARY KEY,
 password varchar(100) NOT NULL);
-LOCK tables 'users' WRITE;
+LOCK tables `users` WRITE;
 INSERT INTO users(username,password) VALUES ('admin',md5('Pa$$w0rd'));
 UNLOCK tables;
