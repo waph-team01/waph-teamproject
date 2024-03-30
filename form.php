49f5c0c2c9a11949cb5a6ad948fcac5d71efc3e1
rampudga <156159822+rampudga@users.noreply.github.com>
2024-03-30 04:34:32 -0400

    Create form.php

---
+++ form.php
@@ -0,0 +1,6 @@
+<form action="index.php" method="POST" class="form login">
+	Username:<input type="text" class="text_feild" name="username" /> <br>
+	Password: <input type="password" class="text_feild" name="password" /> <br>
+	<button class="button" type="submit">
+		Login
+	</button>
