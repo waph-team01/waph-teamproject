89748746d54b261dac91dc2436c3889d131124e4
rampudga <156159822+rampudga@users.noreply.github.com>
2024-03-30 04:32:33 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_POST["username"]) and isset($_POST["password"])) {
+    if (checklogin_mysql($username, $password)) {
+?>
+    <h2> Welcome <?php echo $_POST["username"]; ?> !</h2>
+<?php
+} else {
+    echo "<script>alert('Invalid Username or password please recheck');window.location='form.php';</script>";
+}
+}
+function checklogin_mysql($username, $password) {
+    $mysqli = new mysqli('localhost', 'nakkantm', 'Pa$$w0rd', 'waph');
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    $sql = "SELECT * FROM users WHERE username= ? AND password = MD5(?)";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ss",$username,$password);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    if ($result->num_rows >= 1) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+?>
