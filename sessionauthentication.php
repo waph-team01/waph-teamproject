28a44c3e9e9f8215a34f65a98f9c23b0335a18cb
rampudga <rampudga.mail.uc.edu>
2024-04-01 01:06:21 -0400

    updated chnagenew password.php

---
+++ sessionauthentication.php
@@ -0,0 +1,17 @@
+<?php
+	session_set_cookie_params(15*60, "/","waph-team01.minifacebook.com",TRUE,TRUE);
+	session_start();
+	if(!$_SESSION['authenticated'] or $_SESSION["authenticated"] != TRUE){
+		session_destroy();
+		echo "<script>alert('You have not login, Please login first');</script>";
+		header("Refresh:0; url=form.php");
+		die();
+
+	}
+	if($_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]){
+		session_destroy();
+		echo "<script>alert('Session hijacking attack is detected!');</script>";
+		header("Refresh:0; url=form.php");
+		die();
+	}
+?>
