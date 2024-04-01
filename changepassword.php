a611191d20bf54a53eeb48f5dfd0e0ca85277b8e
rampudga <rampudga.mail.uc.edu>
2024-04-01 01:23:14 -0400

    updated changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -1,6 +1,11 @@
 <?php
 	require "session_auth.php";
 	require "database.php";
+	$token= $_POST['nocsrftoken'];
+	if(!isset($token) or $token!=$_SESSION['nocsrftoken']){
+		echo="CSRF Attack is detected";
+		die();
+	}
 	$username= $_SESSION['username'];
 	$password= $_REQUEST['newpassword'];
 	if(isset($username) and isset($password)){
