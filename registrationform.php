3761de4072d0fee041f084c46ae44b57dc2d08d0
pashamsp <155928223+pashamsp@users.noreply.github.com>
2024-03-31 22:52:51 -0400

    Update registrationform.php

--- registrationform.php
+++ registrationform.php
@@ -17,8 +17,12 @@
   echo "Visited time: " . date("Y-m-d h:i:sa")
 ?>
    <form action="addnewuser.php" method="POST" class="form login">
-     Username: <input type="text" class="text_field" name="username" /> <br>
-     Password: <input type="password" class="text_field" name="password" /> <br>
+      Username: <input type="text" class="text_field" name="username" required
+      pattern="^[\w.-]+@[\w-]+(.[\w-]+)*$"
+      title="Email address is required as username"
+      placeholder="username in email"
+      onchange="this.setCustomerValidity(this.validity.patternMismatch?this.title: '');" /> <br>
+      Password: <input type="password" class="text_field" name="password" /> <br>
      <button class="button" type="submit">Login</button>
    </form>
 </body>
