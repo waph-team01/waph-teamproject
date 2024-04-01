1cf8c26269a346a2444614291dbbc3c85d18b92c
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-01 10:45:59 -0400

    Create changepasswordform.php

---
+++ changepasswordform.php
@@ -0,0 +1,6 @@
+<h1>Change Password Form</h1>
+<form action="changepassword.php" method="POST" class="form login">
+Username: <input type="text" class="text_field" name="username" /> <br>
+Current Password: <input type="password" class="text_field" name="password" /> <br>
+New Password: <input type="password" class="text_field" name="newpassword" /> <br>
+<button class="button" type="submit"> Change password </button>
