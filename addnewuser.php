891e0aa1110f1a02d0048c1142ecd278b13b112f
rampudga <rampudga.mail.uc.edu>
2024-03-31 23:11:59 -0400

    updated database.php

--- addnewuser.php
+++ addnewuser.php
@@ -1,4 +1,5 @@
 <?php 
+	require "database.php";
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	if(isset($username) and isset($password)){
@@ -12,16 +13,5 @@
 else{
 	echo " No username/password provided! ";
 }
-	function addnewuser($username, $password){
-		$mysqli=  new mysqli('localhost','waph_team01','waph@uc!2024','waph_team');
-		if($mysqli->connect_errno){ 
-			printf("Database connction failed; %s\n", $mysqli->connect_error);
-			return FALSE;
-		}
-		$prepared_sql = "INSERT INTO users(username,password) VALUES(?,md5(?));";
-		$stmt = $mysqli->prepare($prepared_sql);
-		$stmt->bind_param("ss", $username,$password);
-		if($stmt->execute())return TRUE;
-		return FALSE;
-	}
+
 ?>
--- database.php
+++ database.php
@@ -1,23 +1,11 @@
 <?php 
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	if(isset($username) and isset($password)){
-		//echo "Debug> got username=$username;got password=$password";
-		if(addnewuser($username, $password)){
-			echo "Registration Succeed";
- 		}else{
-		echo "Registartion Failed!";
-	}
-}
-else{
-	echo " No username/password provided! ";
-}
-	function addnewuser($username, $password){
-		$mysqli=  new mysqli('localhost','waph_team01','waph@uc!2024','waph_team');
+$mysqli=  new mysqli('localhost','waph_team01','waph@uc!2024','waph_team');
 		if($mysqli->connect_errno){ 
 			printf("Database connction failed; %s\n", $mysqli->connect_error);
 			return FALSE;
 		}
+	function addnewuser($username, $password){
+		global mysqli;
 		$prepared_sql = "INSERT INTO users(username,password) VALUES(?,md5(?));";
 		$stmt = $mysqli->prepare($prepared_sql);
 		$stmt->bind_param("ss", $username,$password);
