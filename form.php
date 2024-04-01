0190c636530de5f177a45e3fad4fbbd6c72046e8
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-01 00:58:20 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,10 +1,10 @@
 <form action="index.php" method="POST" class="form login">
-    Username: <input type="text" class="text_field" name="username" /><br> 
-    Password: <input type="password" class="text_field" name="password" /><br> 
+    Username: <input type="text" class="text_field" name="username" /><br>
+    Password: <input type="password" class="text_field" name="password" /><br>
     <button class="button" type="submit">
         Login
     </button>
-    <button class="button" onclick="location.href='registrationform.php';"> 
+    <button class="button" onclick="window.location.href='registrationform.php';"> <!-- Changed onclick to correctly navigate -->
         New User Registration
     </button>
 </form>
