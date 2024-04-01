926db00be125d594ff22ee33039e1d6051cc5f4b
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-01 00:13:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <?php
-session_set_cookie_params(15*60,"/","waph-team01.mini.facebook.com",TRUE,TRUE);
+session_set_cookie_params(15*60, "/", "waph-team01.mini.facebook.com", TRUE, TRUE);
 session_start();
-require "database.php"
-if (isset($_POST["username"]) and isset($_POST["password"])) {
+require "database.php"; 
+
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST["username"]; 
+    $password = $_POST["password"];
+
     if (checklogin_mysql($username, $password)) {
         $_SESSION["authenticated"] = TRUE;
-        $_SESSION["username"] = $_POST["username"];
+        $_SESSION["username"] = $username; 
         $_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
-    }else {
-    session_destroy();
-    echo "<script>alert('Invalid Username or password please recheck');window.location='form.php';</script>";
-    die();
+    } else {
+        session_destroy();
+        echo "<script>alert('Invalid Username or password please recheck');window.location='form.php';</script>";
+        die();
     }
 }
-if (!isset($_SESSION["authenticated"]) or $_SESSION["authenticated"] != TRUE) {
+
+if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] != TRUE) { 
     session_destroy();
-    echo "<script>alert('You have not login. Please login first');</script>";
+    echo "<script>alert('You have not logged in. Please login first');</script>";
     header("Refresh:0; url=form.php");
     die();
 }
-if($_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]){
+
+if ($_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]) {
     session_destroy();
-    echo "<script>alert('session hijack detected')</script>";
+    echo "<script>alert('Session hijack detected')</script>"; 
     header("Refresh:0; url=form.php");
     die();
 }
 ?>
-<h2> Welcome <?php echo htmlentities($_SESSION['usernaem']); ?> !</h2>
-<a href="changepassword.php">Change password</a> | <a href="profile.php">Edit profile</a> | <a href ="logout.php">logout
-</a>
+<h2> Welcome <?php echo htmlentities($_SESSION['username']); ?> !</h2> 
+<a href="changepassword.php">Change password</a> | <a href="profile.php">Edit profile</a> | <a href="logout.php">Logout</a> 
