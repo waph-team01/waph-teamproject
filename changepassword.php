bc3435abc01a8eab2b5a273bf7956dfded4fa099
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-01 02:22:15 -0400

    Update changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -1,22 +1,21 @@
 <?php
 	require "sessionauthentication.php";
 	require "database.php";
-	$token= $_POST['nocsrftoken'];
-	if(!isset($token) or $token!=$_SESSION['nocsrftoken']){
+	$token = $_POST['csrf-token'];
+	if (!isset($token) || $token !== $_SESSION['csrf-token']) {
 		echo "CSRF Attack is detected";
 		die();
 	}
-	$username= $_SESSION['username'];
-	$password= $_REQUEST['newpassword'];
-	if(isset($username) and isset($password)){
-		echo "Debug> changepassword.php got username=$username;got password=$password";
-		if(changepassword($username, $password)){
+	$username = $_SESSION['username'];
+	$newPassword = $_POST['newpassword'];
+	if (isset($username) && isset($newPassword)) {
+		echo "Debug> changepassword.php got username=$username; got password=$newPassword";
+		if (changepassword($username, $newPassword)) {
 			echo "Password has been changed";
- 		}else{
-		echo "Change password failed!";
+ 		} else {
+			echo "Change password failed!";
+		}
+	} else {
+		echo "No username/password provided!";
 	}
-}
-else{
-	echo " No username/password provided! ";
-}
 ?>
