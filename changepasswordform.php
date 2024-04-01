1db31968619a04cf04ca6d567c90ae15d22853d2
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-01 11:08:30 -0400

    Update changepasswordform.php

--- changepasswordform.php
+++ changepasswordform.php
@@ -1,6 +1,6 @@
 <h1>Change Password Form</h1>
 <form action="changepassword.php" method="POST" class="form login">
-Username: <input type="text" class="text_field" name="username" /> <br>
+Username: <input type="text" class="text_field" name="username" value="<?php echo isset($_GET['username']) ? htmlentities($_GET['username']) : ''; ?>" readonly /> <br>
 Current Password: <input type="password" class="text_field" name="password" /> <br>
 New Password: <input type="password" class="text_field" name="newpassword" /> <br>
 <button class="button" type="submit"> Change password </button>
