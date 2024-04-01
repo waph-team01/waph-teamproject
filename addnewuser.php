0ead9a7a4b8f8f08d0dc05bd21c1b1d00aff30d5
pashamsp <155928223+pashamsp@users.noreply.github.com>
2024-04-01 01:10:10 -0400

    Update addnewuser.php

--- addnewuser.php
+++ addnewuser.php
@@ -1,17 +1,20 @@
 <?php 
 	require "database.php";
 	$username = $_POST["username"];
+        $Full Name = $_POST["Full Name"];
+        $Email = $_POST["Email"];
 	$password = $_POST["password"];
+        $Phone Number = $_POST["Phone Number"];
 	if(isset($username) and isset($password)){
 		//echo "Debug> got username=$username;got password=$password";
-		if(addnewuser($username, $password)){
+		if(addnewuser($username, $Full Name, $Email, $password, $Phone Number)){
 			echo "Registration Succeed";
  		}else{
 		echo "Registartion Failed!";
 	}
 }
 else{
-	echo " No username/password provided! ";
+	echo " No username/password/Email/Phone Number/Full Name provided! ";
 }
 
 ?>
