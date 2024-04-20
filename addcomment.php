59afe1e1e937d8a7845fa7d66997c2294eb1bab9
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-20 09:17:51 -0400

    Create addcomment.php

---
+++ addcomment.php
@@ -0,0 +1,16 @@
+<?php
+require "sessionauthentication.php";
+require "database.php";
+
+if (isset($_POST['commentContent']) && isset($_POST['postID'])) {
+    $commentContent = htmlspecialchars($_POST['commentContent']);
+    $postID = $_POST['postID'];
+    $username = $_SESSION['username'];
+
+    if (insertComment($commentContent, $postID, $username)) {
+        echo "Comment added successfully.";
+    } else {
+        echo "Failed to add comment.";
+    }
+} else {
+    echo "No
