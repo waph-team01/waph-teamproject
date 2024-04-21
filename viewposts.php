b96ed9a4d7cc3be06a5f79e5de0f9b60bf2e217a
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-20 22:10:39 -0400

    Update viewposts.php

--- viewposts.php
+++ viewposts.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Main Page</title>
-  <link rel="stylesheet" href="styles.css">
   <style>
     .post-container {
       margin-bottom: 20px;
