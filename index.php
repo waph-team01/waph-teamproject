bd2ecee1799d0eae37a7924ee352f8c3f807f084
Vihasith137 <155859844+Vihasith137@users.noreply.github.com>
2024-04-21 12:06:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
-<?php
-session_set_cookie_params(15*60, "/", "waph-team01.mini.facebook.com", TRUE, TRUE);
-session_start();
-require "database.php"; 
-
-if (isset($_POST["username"]) && isset($_POST["password"])) {
-    $username = $_POST["username"]; 
-    $password = $_POST["password"];
-
-    if (checklogin_mysql($username, $password)) {
-        $_SESSION["authenticated"] = TRUE;
-        $_SESSION["username"] = $username; 
-        $_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
-    } else {
-        session_destroy();
-        echo "<script>alert('Invalid Username or password please recheck');window.location='form.php';</script>";
-        die();
-    }
-}
-
-if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] != TRUE) { 
-    session_destroy();
-    echo "<script>alert('You have not logged in. Please login first');</script>";
-    header("Refresh:0; url=form.php");
-    die();
-}
-
-if ($_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]) {
-    session_destroy();
-    echo "<script>alert('Session hijack detected')</script>"; 
-    header("Refresh:0; url=form.php");
-    die();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
