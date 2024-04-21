e48ab95b612ed72651c045c5597d23c74e518fb6
Vihasith137 <155859844+Vihasith137@users.noreply.github.com>
2024-04-20 23:30:47 -0400

    Update addcomment.php

--- addcomment.php
+++ addcomment.php
@@ -75,7 +75,7 @@
           echo "No comment content was provided";
       }
 
-      header("Refresh:3; url=viewposts.php");
+      header("Refresh:2; url=viewposts.php");
       ?>
     </div>
   </div>
