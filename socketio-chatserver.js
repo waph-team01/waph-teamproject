490a07ce050bb06bdbe3ce1edc871939ea2fc431
Tulasiram Nakkanaboina <155696605+nakkantm-uc@users.noreply.github.com>
2024-04-25 01:31:32 -0400

    Update socketio-chatserver.js

--- socketio-chatserver.js
+++ socketio-chatserver.js
@@ -1,23 +1,30 @@
-/* A Simple HTTP server with socket.IO in Node.js by Phu Phung */
-var http = require('http'), fs = require('fs');
-var httpServer = http.createServer(httphandler);
-var socketio = require('socket.io')(httpServer);
-var port = 8080;
-httpServer.listen(port); 
-console.log("HTTPS server is listenning on port "+ port);
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
 
-function httphandler (request, response) {
+const port = 8080;
+
+const httpServer = http.createServer(httphandler);
+const io = socketio(httpServer);
+
+httpServer.listen(port);
+console.log("HTTP server is listening on port " + port);
+
+function httphandler(request, response) {
   response.writeHead(200); // 200 OK 
-  //ensure you have the front-end UI client.html
-  var clientUI_stream = fs.createReadStream('./client.html');
+  // Ensure you have the front-end UI client.html
+  const clientUI_stream = fs.createReadStream('./client.html');
   clientUI_stream.pipe(response);
 }
-socketio.on('connection', function (socketclient) {
-  console.log("A new socket.IO client is connected: "+ 
-	       socketclient.client.conn.remoteAddress+": "+
-	       socketclient.id);
-		socketclinet.on("message",(data)=>{
-			console.log("Received data: "+data);
-});
 
-socketio.emit("message",data);
+io.on('connection', function (socketclient) {
+  console.log("A new socket.IO client is connected: " +
+    socketclient.client.conn.remoteAddress + ": " +
+    socketclient.id);
+
+  socketclient.on("message", (data) => {
+    console.log("Received data: " + data);
+    // Emit the received message to all clients
+    io.emit("message", data);
+  });
+});
